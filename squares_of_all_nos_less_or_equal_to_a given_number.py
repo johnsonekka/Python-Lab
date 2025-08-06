3dad4ad8a586238b2d737b6ebf7fbd3da7678476
johnsonekka <johnsonekka@hotmail.com>
2025-08-06 15:39:18 +0530

    python lab 2

---
+++ squares_of_all_nos_less_or_equal_to_a given_number.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: "))
+
+print(f"\nsquare of numbers from 1 to {num}:\n")
+for i in range(1,num + 1):
+    print(f"{i}^2 = {i * i}")
+    
---
+++ table_of_a_given_number.py
@@ -0,0 +1,5 @@
+num = int(input("enter a number:"))
+
+print(f"multiplication Table of {num}:")
+for i in range(1,11):
+    print(f"{num}x {i}= {num*i}")
